f42add50f6f9285a1b54f11e6d88d98ff495edd3
Hovhannes23 <twinkle.groupbz@gmail.com>
2021-01-12 15:47:34 +0300

    added entities "Product" and "Manufacturer"

---
+++ src/main/java/com/example/mobileShop/Manufacturer.java
@@ -0,0 +1,59 @@
+package com.example.mobileShop;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="manufacturers")
+public class Manufacturer {
+
+    private Long id;
+    private String country;
+    private String email;
+    private String name;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manufacturer_id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Column(name = "manufacturer_country")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    @Column(name = "manufacturer_email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Column(name = "manufacturer_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Manufacturer(String country, String email, String name) {
+        this.country = country;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Manufacturer() {
+    }
+}
--- src/main/java/com/example/mobileShop/Product.java
+++ src/main/java/com/example/mobileShop/Product.java
@@ -1,4 +1,63 @@
 package com.example.mobileShop;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="products")
 public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+    private String description;
+    private Manufacturer manufacturer;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Column(name = "product_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "product_price")
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Column(name = "product_description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id")
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
