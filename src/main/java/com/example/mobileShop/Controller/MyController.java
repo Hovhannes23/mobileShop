f65c389020f0b4f7fd719686e6741a72eb2598ab
Hovhannes23 <twinkle.groupbz@gmail.com>
2021-01-12 16:33:28 +0300

    application started well

--- src/main/java/com/example/mobileShop/Controller/MyController.java
+++ src/main/java/com/example/mobileShop/Controller/MyController.java
@@ -1,10 +1,14 @@
 package com.example.mobileShop.Controller;
 
 
+import com.example.mobileShop.Entity.Product;
 import com.example.mobileShop.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -13,7 +17,10 @@ public class MyController {
     private ProductService service;
 
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+        List<Product> listProducts = service.listAll();
+
+        model.addAttribute("listProducts", listProducts);
         return "homePage";
     }
 }
